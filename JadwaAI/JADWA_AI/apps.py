f184bd7efe9ab94b5aac3c65fbcde975a9c192e8
MAC <mac@Suras-MacBook-Pro.local>
2026-01-20 10:57:55 +0300

    Initial commit with new folder

---
+++ JadwaAI/JADWA_AI/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class JadwaAiConfig(AppConfig):
+    name = 'JADWA_AI'
---
+++ JadwaAI/JADWA_AI/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Project(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    project_name = models.CharField(max_length=200)
+    budget = models.FloatField()
+    duration = models.IntegerField()
+    location = models.CharField(max_length=100)
+    sector = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.project_name
+
+
+class AnalysisResult(models.Model):
+    project = models.OneToOneField(Project, on_delete=models.CASCADE)
+    feasibility_score = models.FloatField()
+    recommendation = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.project
