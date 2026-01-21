e796ca1013ae4e3cd51022486f70f265afb0bba6
JoriAlshoshan <jori.alshoshan@gmail.com>
2026-01-21 19:18:11 +0300

    Initial backend setup: models, migrations, AI integration

--- JadwaAI/JADWA_AI/models.py
+++ JadwaAI/JADWA_AI/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 class Project(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     project_name = models.CharField(max_length=200)
@@ -22,4 +21,4 @@ class AnalysisResult(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.project
+        return f"Result for {self.project.project_name}"
