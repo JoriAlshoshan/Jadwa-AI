670fb52354dd1b04567851b2a9d1650f50953050
JoriAlshoshan <jori.alshoshan@gmail.com>
2026-01-29 06:58:26 +0300

    Analysis page

--- JadwaAI/JadwaAI/urls.py
+++ JadwaAI/JadwaAI/urls.py
@@ -4,13 +4,10 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # App routes (Landing, pages, etc.)
     path('', include('JADWA_AI.urls')),
 
-    # Authentication
     path('accounts/', include('django.contrib.auth.urls')),
     path("i18n/", include("django.conf.urls.i18n")),
 
-    #  Put analysis under a clear prefix
     path("analysis/", include("analysis.urls")),
 ]
--- JadwaAI/analysis/urls.py
+++ JadwaAI/analysis/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("run/<int:project_id>/", views.run_analysis, name="run_analysis"),
     path("result/<int:result_id>/", views.analysis_result, name="analysis_result"),
+    path("recs-loading/<int:result_id>/", views.recs_loading, name="recs_loading"),
+    path("recs-status/<int:result_id>/", views.recs_status, name="recs_status"),
     path("recommend/<int:result_id>/", views.generate_recs, name="generate_recs"),
 ]
