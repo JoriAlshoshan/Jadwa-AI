beb376d9b9176d01f80ade0a1f7cacb2b87577c8
Rahaf <r22581919@gmail.com>
2026-02-02 22:49:21 +0300

    Change CSV path to use settings.BASE_DIR
    Updated the CSV file path to use a dynamic path from settings.

--- JadwaAI/JADWA_AI/fill_economic_indicator.py
+++ JadwaAI/JADWA_AI/fill_economic_indicator.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 def calculate_update_economic_indicator():
     csv_path = os.path.join(settings.BASE_DIR, 'dataset', 'jadwa_ai_final_dataset.csv')
-    df = pd.read_csv(r"C:\Users\رهف\Desktop\JadwaAI project\Jadwa-AI\JadwaAI\dataset\jadwa_ai_final_dataset.csv")
+    df = pd.read_csv("")
 
     df['establishments_norm'] = df['عدد المنشآت'] / df['عدد المنشآت'].max()
     df['number_of_saudi_employees_norm'] = df['عدد العاملين السعوديين'] / df['عدد العاملين السعوديين'].max()
