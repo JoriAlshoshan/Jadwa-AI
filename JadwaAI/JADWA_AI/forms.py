9ec2988ef3cd97e3f1b3d49e7c8baac2ebf609f9
Rahaf <r22581919@gmail.com>
2026-02-02 21:41:11 +0300

    add forms

--- JadwaAI/JADWA_AI/forms.py
+++ JadwaAI/JADWA_AI/forms.py
@@ -1,8 +1,7 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, SetPasswordForm
-from django.contrib.auth import get_user_model  
-
-User = get_user_model()
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from .models import Projects
 
 class JadwaUserCreationForm(UserCreationForm):
     email = forms.EmailField(
@@ -10,11 +9,13 @@ class JadwaUserCreationForm(UserCreationForm):
         widget=forms.EmailInput(attrs={'placeholder': 'Email', 'class': 'form-input'})
     )
     
-    class Meta(UserCreationForm.Meta): 
-        model = User 
-        fields = ("username", "email") 
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
         widgets = {
             "username": forms.TextInput(attrs={'placeholder': 'Username', 'class': 'form-input'}),
+            "password1": forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'form-input'}),
+            "password2": forms.PasswordInput(attrs={'placeholder': 'Confirm Password', 'class': 'form-input'}),
         }
 
 class JadwaAuthenticationForm(AuthenticationForm):
@@ -25,19 +26,6 @@ class JadwaAuthenticationForm(AuthenticationForm):
         widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'form-input'})
     )
 
-class ForgotPasswordForm(forms.Form):
-    email = forms.EmailField(label="Email", widget=forms.EmailInput(attrs={'class': 'form-input'}))
-
-class OTPForm(forms.Form):
-        otp = forms.CharField(
-        label="OTP Code", 
-        max_length=6, 
-        min_length=6, 
-        widget=forms.TextInput(attrs={'class': 'form-input', 'placeholder': '123456'})
-    )
-
-class ResetPasswordForm(SetPasswordForm):
-    pass
 class ProjectInformationForm(forms.ModelForm):
     class Meta:
         model = Projects
